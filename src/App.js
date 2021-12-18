764fabbdadbd3b3ec20907bb3dd5abf858ce1720
Vlad <64969216+dreamkaman@users.noreply.github.com>
2021-12-18 17:18:53 +0200

    cloned next

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ContactForm from './ContactForm';
+import Section from './Section';
+import ContactList from './ContactList';
+import InputElement from './ContactForm/InputElement';
+
+import styles from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  filterContacts = text =>
+    this.state.contacts.filter(contact => contact.name.toUpperCase().includes(text.toUpperCase()));
+
+  handleDelete = event => {
+    const newContacts = this.state.contacts.filter(contact => contact.id !== event.target.id);
+
+    this.setState(() => ({ contacts: newContacts, filter: '' }));
+  };
+
+  addContact = ({ id, name, number }) =>
+    this.setState(prevstate => ({
+      contacts: [...prevstate.contacts, { id, name, number }],
+      filter: '',
+    }));
+
+  handleChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm onFormSubmit={this.addContact} contacts={this.state.contacts} />
+        </Section>
+        <Section title="Contacts">
+          <InputElement
+            className={styles.filter}
+            name="filter"
+            type="text"
+            value={this.state.filter}
+            text="Find contacts by name"
+            onChange={this.handleChange}
+          />
+          <ContactList
+            contacts={this.filterContacts(this.state.filter)}
+            onClick={this.handleDelete}
+          />
+        </Section>
+      </>
+    );
+  }
 }
 
 export default App;
